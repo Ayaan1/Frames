8c3b419c46265cc8c243a277be20d7e3036635a8
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2017-04-02 13:26:09 -0500

    Minor changes.

--- library/src/main/java/jahirfiquitiva/libs/frames/activities/base/ThemedActivity.java
+++ library/src/main/java/jahirfiquitiva/libs/frames/activities/base/ThemedActivity.java
@@ -21,6 +21,7 @@ import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 
 import java.util.ArrayList;
 
